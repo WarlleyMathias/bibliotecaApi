b09bce7ab03c33e2d53dd7bd0bb9d72926788905
Warlley <warlleymathias1.1@gmail.com>
2026-09-02 23:33:42 -0300

    pequenos ajustes.

--- src/main/java/com/warlley/biblioteca/model/Usuario.java
+++ src/main/java/com/warlley/biblioteca/model/Usuario.java
@@ -23,15 +23,8 @@ public class Usuario {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
