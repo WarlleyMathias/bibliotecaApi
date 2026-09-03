c3a24d69b47ebf5f0a82ba09697d78d462e689f7
Warlley <warlleymathias1.1@gmail.com>
2026-09-02 23:34:30 -0300

    pequenos ajustes.

--- src/main/java/com/warlley/biblioteca/model/Livro.java
+++ src/main/java/com/warlley/biblioteca/model/Livro.java
@@ -29,26 +29,6 @@ public class Livro {
         return titulo;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
     public boolean isDisponivel() {
         return disponivel;
     }
